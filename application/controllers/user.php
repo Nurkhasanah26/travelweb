1a8faa3f1c07e948cdbaa46e7712a6f68f540be8
Nurkhasanah26 <nurrkhasanah26@gmail.com>
2018-05-07 16:15:28 +0700

    second commit

---
+++ application/controllers/user.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function form()
+	{
+		$this->load->view('view.php');
+
+	}
+
+}
+
+/* End of file user.php */
+/* Location: ./application/controllers/user.php */
---
+++ application/views/view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	Username <br>
+	<input type="text" name="username"><br>
+	Password <br>
+	<input type="password" name="password"><br>
+	Level
+	<select>
+		<option value="admin">Admin</option>
+		<option value="user">User</option>
+	</select>
+	<input type="submit" name="simpan" value="Simpan">
+</form>
+</body>
+</html>
